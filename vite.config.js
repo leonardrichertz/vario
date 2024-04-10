3b73e5f2da20d103c5ff51fbd031944bbdbf64f7
Leonard Richertz <leo.richertz@gmx.de>
2024-04-10 12:15:02 +0200

    initialize functionality

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vario/',
+  // Uncomment the following for github pages deployment
+  // base: '/vario/',
 })
