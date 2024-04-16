51f03c24c6c1fe2e1c44d943ea6a1fa25648d3ab
leonardrichertz <leonardrichertz@users.noreply.github.com>
2024-04-16 08:53:08 +0000

    deploy: 876dccbe2726cf935678f08a441fffac5c5d654d

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#start").html("Device orientation and geolocation data");
+    $("#start").text("Device orientation and geolocation data");
     // Function to handle device orientation data
     function handleOrientation(event) {
         var alpha;
@@ -17,19 +17,19 @@ $(document).ready(function(){
         $("#compass").css("transform", "rotate(" + alpha + "deg)");
 
         // Display device orientation data
-        $("#orientationData").html("Alpha: " + alpha + "<br>Beta: " + beta + "<br>Gamma: " + gamma);
+        $("#orientationData").html("<br>Alpha: " + alpha + "<br>Beta: " + beta + "<br>Gamma: " + gamma);
     }
 
     // Function to handle geolocation data
     function handleGeolocation(position) {
         // Display geolocation data
-        $("#geolocationData").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+        $("#geolocationData").html("<br>Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
     }
 
     // Function to handle geolocation errors
     function handleError(error) {
         // Handle error
-        $("#geolocationData").html("Geolocation error: " + error.message);
+        $("#geolocationData").text("Geolocation error: " + error.message);
     }
 
     // Check if the device supports DeviceOrientationEvent
@@ -40,7 +40,7 @@ $(document).ready(function(){
     } else {
         // Device orientation not supported
         console.log("Device orientation not supported.");
-        $("#orientationData").html("Device orientation not supported.");
+        $("#orientationData").text("Device orientation not supported.");
     }
 
     // Get geolocation data continuously
@@ -48,6 +48,6 @@ $(document).ready(function(){
         var watchId = navigator.geolocation.watchPosition(handleGeolocation, handleError);
     } else {
         // Geolocation not supported
-        $("#geolocationData").html("Geolocation not supported.");
+        $("#geolocationData").text("Geolocation not supported.");
     }
 });
