2134d6c51bb994ea80033e51f1b71d3a8ee0a671
Leonard Richertz <leo.richertz@gmx.de>
2024-04-16 10:05:05 +0200

    changed function.js

--- src/functions.js
+++ src/functions.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         var alpha    = Math.round(event.alpha * 100) / 100; // rotation around z-axis
         var beta     = Math.round(event.beta * 100) / 100;  // rotation around x-axis
         var gamma    = Math.round(event.gamma * 100) / 100; // rotation around y-axis
-        $("#compass").style.transform = "rotate(" + alpha + "deg)";
+        $("#compass").css("transform", "rotate(" + alpha + "deg)");
 
         // Display device orientation data
         $("#orientationData").html("Absolute: " + absolute + "<br>Alpha: " + alpha + "<br>Beta: " + beta + "<br>Gamma: " + gamma);
