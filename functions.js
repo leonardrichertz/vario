508fe98e60fdfeac0890f6601dd66c9d996fff07
leonardrichertz <leonardrichertz@users.noreply.github.com>
2024-04-16 08:37:45 +0000

    deploy: cfc4f168816734d13c7f481706d58149e7f3e0a5

--- functions.js
+++ functions.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
     // Function to handle device orientation data
     function handleOrientation(event) {
-        var absolute = Math.round(event.absolute * 100) / 100;  
-        var alpha    = Math.round(event.alpha * 100) / 100; // rotation around z-axis
+        var alpha;
+        if (event.webkitCompassHeading !== undefined) {
+            alpha = event.webkitCompassHeading; // iOS
+        } else {
+            alpha = event.alpha; // non-iOS
+        }
+        
+        alpha = Math.round(alpha * 100) / 100; // Normalize value
+        
         var beta     = Math.round(event.beta * 100) / 100;  // rotation around x-axis
         var gamma    = Math.round(event.gamma * 100) / 100; // rotation around y-axis
-        console.log($("#compass"))
+
         $("#compass").css("transform", "rotate(" + alpha + "deg)");
 
         // Display device orientation data
-        $("#orientationData").html("Absolute: " + absolute + "<br>Alpha: " + alpha + "<br>Beta: " + beta + "<br>Gamma: " + gamma);
+        $("#orientationData").html("Alpha: " + alpha + "<br>Beta: " + beta + "<br>Gamma: " + gamma);
     }
 
     // Function to handle geolocation data
@@ -23,17 +30,6 @@ $(document).ready(function(){
         // Handle error
         $("#geolocationData").html("Geolocation error: " + error.message);
     }
-
-    // Function to handle device motion data
-    // function handleDeviceMotion(event) {
-    //     var acceleration = event.acceleration;
-    //     var accelerationIncludingGravity = event.accelerationIncludingGravity;
-    //     var rotationRate = event.rotationRate;
-    //     console.log(acceleration, accelerationIncludingGravity, rotationRate)
-
-    //     // Display device motion data
-    //     $("#deviceMotionData").html("Acceleration: " + JSON.stringify(acceleration) + "<br>Acceleration including gravity: " + JSON.stringify(accelerationIncludingGravity) + "<br>Rotation rate: " + JSON.stringify(rotationRate));
-    // }
 
     // Check if the device supports DeviceOrientationEvent
     if (window.DeviceOrientationEvent) {
@@ -53,12 +49,4 @@ $(document).ready(function(){
         // Geolocation not supported
         $("#geolocationData").html("Geolocation not supported.");
     }
-
-    // // Toggle device motion data display
-    // $("#toggleButton").click(function(){
-    //     $("#deviceMotionData").toggle();
-    // });
-
-    // // Listen for device motion events
-    // window.addEventListener('devicemotion', handleDeviceMotion, false);
 });
