3b21e4faa46d7aec05477968c09ebdb84f5b88ea
Leonard Richertz <leo.richertz@gmx.de>
2024-04-23 22:34:23 +0200

    Speed tryout

--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
     var map = L.map('map').setView([0, 0], 13);
-    var oldPosition;
+    var marker;
+
     // Add Tile layer for map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    marker = L.marker([0, 0]).addTo(map).bindPopup("You are here");
 
     $("#start").text("Device orientation and geolocation data");
     // Function to handle device orientation data
@@ -32,28 +35,7 @@ $(document).ready(function () {
         $("#orientationData").html("<br>heading: " + heading + "<br>Alpha: " + alpha + "<br>Beta: " + beta + "<br>Gamma: " + gamma);
     }
 
-    function updateLocationOnMap(position) {
-        var latlng = [position.coords.latitude, position.coords.longitude];
-        map.setView(latlng);
-        L.marker(latlng).addTo(map)
-            .bindPopup("You are here").openPopup();
-
-        oldPosition = position;
-    }
-
-    function calculateGroundSpeed(position) {
-        var speed = 0;
-        var latlng1 = L.latLng(oldPosition.coords.latitude, oldPosition.coords.longitude);
-        var latlng2 = L.latLng(position.coords.latitude, position.coords.longitude);
-        var time1 = oldPosition.timestamp;
-        var time2 = position.timestamp;
-        var distance = latlng1.distanceTo(latlng2); // Distance in meters
-        var timeDiff = (time2 - time1) / 1000; // Time difference in seconds
-        speed = distance / timeDiff; // Speed in meters per second
-        console.log(speed);
-        return speed;
-    }
-
+  
     function handleMotion(event) {
         // Display acceleration data
         $("#motionData").html("Acceleration: <br>x: " + event.acceleration.x + "<br>y: " + event.acceleration.y + "<br>z: " + event.acceleration.z);
@@ -61,12 +43,13 @@ $(document).ready(function () {
 
     // Function to handle geolocation data
     function handleGeolocation(position) {
-        // update the Location on the map
-        updateLocationOnMap(position);
-        console.log(position.coords);
-        console.log(position.coords.speed); // Current speed
+        var latlng = [position.coords.latitude, position.coords.longitude];
+        marker.setLatLng(latlng).update();
+        map.setView(latlng);
+
         // Display geolocation data
-        $("#geolocationData").html("<br>Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);       
+        $("#geolocationData").html("<br>Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+
         // Display current speed
         if (position.coords.speed !== null && !isNaN(position.coords.speed)) {
             var speed = position.coords.speed;
@@ -144,5 +127,4 @@ $(document).ready(function () {
         // Geolocation not supported
         $("#geolocationData").text("Geolocation not supported.");
     }
-
 });
