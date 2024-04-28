0a01dcfd56a0996443cb61034b5f3cb1c56de293
Leonard Richertz <leo.richertz@gmx.de>
2024-04-28 18:52:48 +0200

    test altitude

---
+++ src/altitude.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    function handleError(){
+        $("#altitudeData").text("Error getting altitude.");
+    }
+    const options = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000,
+    };
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(function (position) {
+            var altitude = position.coords.altitude;
+            $("#altitudeData").text("Altitude: " + altitude);
+            // Use the altitude value here
+        }, handleError, options);
+    }
+    else {
+        $("#altitudeData").text("Geolocation not supported.");
+    }
+});
