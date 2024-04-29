7d31c56b3f2ab4405026bf40c625a8f018b80c1f
leonardrichertz <leonardrichertz@users.noreply.github.com>
2024-04-29 08:43:13 +0000

    deploy: 13cf3f0a5e45398d31b6030c82599e1dda2d9f35

--- dev/altitude.js
+++ dev/altitude.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    const thresholdRotation = 2.5; // Define threshold for rotation
     function handleError() {
         $("#altitudeData").text("Error getting altitude.");
     }
@@ -17,4 +18,77 @@ $(document).ready(function () {
     else {
         $("#altitudeData").text("Geolocation not supported.");
     }
+    function handleMotion(evt){
+        evt.interval;
+        // Handle motion data here
+
+    }
+
+    function handleOrientation(evt){
+        if (evt.gamma > thresholdRotation || evt.gamma < -thresholdRotation){
+            // Handle rotation here
+        }
+        if (evt.beta > thresholdRotation || evt.beta < -thresholdRotation){
+            // Handle rotation here
+        }
+        // We do not have to use alpha since it only represents the compass direction (rotaion around z-axis)
+        
+
+
+    }
+
+    $("#requestMotionPermissionButton").click(function () {
+        if (window.DeviceMotionEvent) {
+            $("#motion").text("Device motion supported.");
+            if (typeof (DeviceMotionEvent) !== 'undefined' && typeof (DeviceMotionEvent.requestPermission) === 'function') {
+                DeviceMotionEvent.requestPermission().then(permissionState => {
+                    if (permissionState === 'granted') {
+                        window.addEventListener('devicemotion', handleMotion, handleMotionError);
+                    }
+                    else {
+                        alert('Permission not granted for DeviceMotion');
+                    }
+                }).catch(console.error);
+            }
+            else {
+                $("#motionInfo").text("No need to request permission for DeviceMotion");
+                window.addEventListener('devicemotion', handleMotion, handleMotionError);
+            }
+        }
+        else {
+            $("#motionInfo").text("Device motion not supported.");
+        }
+    });
+
+    $("#requestOrientationPermissionButton").click(function () {
+        // Check if the device supports DeviceOrientationEvent
+        if (window.DeviceOrientationEvent) {
+            $("#orientation").text("Device orientation supported.");
+            // Check if we need to request permission (iOS 13+)
+            if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+                $("#orientationInfo").text("Requesting permission for DeviceOrientation");
+                // The request permission is only needed for iOS 13+ devices and it is only available in Safari.
+                DeviceOrientationEvent.requestPermission()
+                    .then(permissionState => {
+                        if (permissionState === 'granted') {
+                            $("#permission").text("Permission granted for DeviceOrientation");
+                            // Permission granted, add event listener
+                            window.addEventListener('deviceorientation', handleOrientation);
+                        } else {
+                            $("#permission").text("Permission not granted for DeviceOrientation");
+                            // Permission not granted
+                            alert('Permission not granted for DeviceOrientation');
+                        }
+                    })
+                    .catch(console.error);
+            } else {
+                $("#orientationInfo").text("No need to request permission for DeviceOrientation");
+                // No need to request permission, add event listener directly
+                window.addEventListener('deviceorientation', handleOrientation);
+            }
+        } else {
+            $("#orientation").text("Device orientation not supported.");
+        }
+    });
+
 });
