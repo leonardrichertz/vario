7ec8a54cc15808b2c92abaeb8dc586252e2abc8e
leonardrichertz <leonardrichertz@users.noreply.github.com>
2024-05-20 17:03:47 +0000

    deploy: a7c6db43628f661554618affa07d5adf588fa6af

---
+++ dev/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'pwa-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/functions.js',
+  '/assets/compass.png',
+  'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (!cacheWhitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
