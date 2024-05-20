174b14e26dfe002dfb963c13e1b0645d15253c6d
Leonard Richertz <leo.richertz@gmx.de>
2024-05-20 19:23:03 +0200

    trying out PWA

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'pwa-app-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/functions.js',
-  '/assets/compass.png',
+  '/vario/dev/',
+  '/vario/dev/index.html',
+  '/vario/dev/functions.js',
+  '/vario/dev/assets/compass.png',
   'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
