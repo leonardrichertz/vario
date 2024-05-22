8aa4f165fd090e0ab3e47b427e8811d5b9d0ffb6
Leonard Richertz <leo.richertz@gmx.de>
2024-05-22 14:39:05 +0200

    changing map

--- src/scripts/speed.js
+++ src/scripts/speed.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {  
    
    var velocity = { x: 0, y: 0, z: 0 }; // Initialize velocity
-    var displacement = { x: 0, y: 0, z: 0 }; // Initialize displacement
-    var previousTimestamp = null; // Variable to store the previous timestamp
-
-
+   var displacement = { x: 0, y: 0, z: 0 }; // Initialize displacement
+   var previousTimestamp = null; // Variable to store the previous timestamp
 
 function handleMotion(event) {
     var acceleration = event.acceleration;
