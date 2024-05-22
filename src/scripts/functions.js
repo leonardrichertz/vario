66ebd13d5b0db19d09f76380f01db6b2d30390c0
Leonard Richertz <leo.richertz@gmx.de>
2024-05-22 15:11:09 +0200

    changing map

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -33,8 +33,8 @@ $(document).ready(function () {
         }
         else {
             alpha = event.alpha; //standard
-            var heading = 360 - alpha; //heading [0, 360)
-            heading = Math.round(alpha * 100) / 100; // Normalize value
+            var heading = compassHeading(alpha, event.beta, event.gamma); //heading [0, 360)
+            // heading = Math.round(alpha * 100) / 100; // Normalize value
             $("#orientationData").html("Not supported webkitCompassHeading");
             $("#compass").css("transform", "rotate(" + heading + "deg)");
         }
@@ -44,6 +44,43 @@ $(document).ready(function () {
         // Display device orientation data
         $("#orientationData").html("<br>heading: " + heading + "<br>Alpha: " + alpha + "<br>Beta: " + beta + "<br>Gamma: " + gamma);
     }
+
+    function compassHeading(alpha, beta, gamma) {
+
+        // Convert degrees to radians
+        var alphaRad = alpha * (Math.PI / 180);
+        var betaRad = beta * (Math.PI / 180);
+        var gammaRad = gamma * (Math.PI / 180);
+      
+        // Calculate equation components
+        var cA = Math.cos(alphaRad);
+        var sA = Math.sin(alphaRad);
+        var cB = Math.cos(betaRad);
+        var sB = Math.sin(betaRad);
+        var cG = Math.cos(gammaRad);
+        var sG = Math.sin(gammaRad);
+      
+        // Calculate A, B, C rotation components
+        var rA = - cA * sG - sA * sB * cG;
+        var rB = - sA * sG + cA * sB * cG;
+        var rC = - cB * cG;
+      
+        // Calculate compass heading
+        var compassHeading = Math.atan(rA / rB);
+      
+        // Convert from half unit circle to whole unit circle
+        if(rB < 0) {
+          compassHeading += Math.PI;
+        }else if(rA < 0) {
+          compassHeading += 2 * Math.PI;
+        }
+      
+        // Convert radians to degrees
+        compassHeading *= 180 / Math.PI;
+      
+        return compassHeading;
+      
+      }
 
     function handleMotion(event) {
         if (previousTimestamp === null) {
