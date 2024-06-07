ef7b11c6e05599f26727b1fec1556b0b89e1b827
MatthiasSimon03 <MatthiasSimon03@users.noreply.github.com>
2024-06-07 15:24:16 +0000

    deploy: 681f8456010819b2ad1d2d916621fe2dacce39b9

--- dev/scripts/altitudeTest.js
+++ dev/scripts/altitudeTest.js
@@ -1,7 +1,30 @@
 $(document).ready(function () {
 
-    import { getOS } from '../utils/operatingSystem.js';
+    function getOS() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
+        if (/windows phone/i.test(userAgent)) {
+            return "Windows Phone";
+        }
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+        if (/Win/i.test(userAgent)) {
+            return "Windows";
+        }
+        if (/Mac/i.test(userAgent)) {
+            return "MacOS";
+        }
+        if (/Linux/i.test(userAgent)) {
+            return "Linux";
+        }
+        return "unknown";
+    }
+
+    
   let initialAltitude = 0;
   let currentAltitude = 0;
   let gammaShift = 0;
