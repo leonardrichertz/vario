8113f656372534623b51f094595643b8d93c0a0c
Leonard Richertz <leo.richertz@gmx.de>
2024-06-12 18:00:03 +0200

    testing out integration

--- src/utils/routeUtils.js
+++
@@ -1,8 +0,0 @@
-export function calculateFlownTime(trackingStartTime) {
-    if (trackingStartTime === null) {
-        return 0;
-    }
-    var currentTime = new Date().getTime();
-    var elapsedTime = currentTime - trackingStartTime;
-    return Math.floor(elapsedTime / 1000); // Convert milliseconds to seconds
-}
