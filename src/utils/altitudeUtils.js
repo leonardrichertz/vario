a34e447656293f6495c20c33fdb6f39bc7a7bd69
Leonard Richertz <leo.richertz@gmx.de>
2024-07-04 11:09:41 +0200

    added change indicator in html

--- src/utils/altitudeUtils.js
+++ src/utils/altitudeUtils.js
@@ -4,43 +4,43 @@ export function changeAltitudeIcon(speed) {
 
     // Highlight the correct bar based on speed
     switch (true) {
-        case speed > 1.5:
+        case speed > 0.8:
             $("#bar1").addClass("bar-light");
             break;
-        case speed > 1:
+        case speed > 0.5:
             $("#bar2").addClass("bar-light");
             break;
-        case speed > 0.8:
+        case speed > 0.4:
             $("#bar3").addClass("bar-light");
             break;
-        case speed > 0.6:
+        case speed > 0.3:
             $("#bar4").addClass("bar-light");
             break;
-        case speed > 0.4:
+        case speed > 0.2:
             $("#bar5").addClass("bar-light");
             break;
-        case speed > 0.2:
+        case speed > 0.1:
             $("#bar6").addClass("bar-light");
             break;
-        case speed <= 0.2 && speed > -0.2:
+        case speed <= 0.1 && speed > -0.1:
             $("#bar7").addClass("bar-light");
             break;
-        case speed < -0.2 && speed > -0.4:
+        case speed < -0.1 && speed > -0.2:
             $("#bar8").addClass("bar-light");
             break;
-        case speed < -0.4 && speed > -0.6:
+        case speed < -0.2 && speed > -0.3:
             $("#bar9").addClass("bar-light");
             break;
-        case speed < -0.6 && speed > -0.8:
+        case speed < -0.3 && speed > -0.4:
             $("#bar10").addClass("bar-light");
             break;
-        case speed < -0.8 && speed > -1:
+        case speed < -0.4 && speed > -0.5:
             $("#bar11").addClass("bar-light");
             break;
-        case speed <= -1 && speed > -1.5:
+        case speed <= -0.5 && speed > -0.8:
             $("#bar12").addClass("bar-light");
             break;
-        case speed <= -1.5:
+        case speed <= -0.8:
             $("#bar13").addClass("bar-light");
             break;
         default:
