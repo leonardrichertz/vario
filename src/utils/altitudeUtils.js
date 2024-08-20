c90942266dae1f45eedbaf17b34f1a15b910e868
Leonard Richertz <leo.richertz@gmx.de>
2024-08-20 12:57:46 +0200

    added multiple soundProfiles for increasing of descreasing pitch of sound

--- src/utils/altitudeUtils.js
+++ src/utils/altitudeUtils.js
@@ -7,46 +7,49 @@ export function changeAltitudeIcon(speed) {
     switch (true) {
         case speed > 0.8:
             $("#bar1").addClass("bar-light");
-            break;
+            return 'ascentProfile1';
         case speed > 0.5:
             $("#bar2").addClass("bar-light");
-            break;
+            return 'ascentProfile2';
         case speed > 0.4:
             $("#bar3").addClass("bar-light");
-            break;
+            return 'ascentProfile3';
         case speed > 0.3:
             $("#bar4").addClass("bar-light");
-            break;
+            return 'ascentProfile4';
         case speed > 0.2:
             $("#bar5").addClass("bar-light");
-            break;
+            return 'ascentProfile5';
         case speed > 0.1:
             $("#bar6").addClass("bar-light");
-            break;
-        case speed <= 0.1 && speed > -0.1:
+            return 'ascentProfile6';
+        case speed <= 0.1 && speed > 0:
             $("#bar7").addClass("bar-light");
-            break;
+            return 'ascentProfile7';
+        case speed <= 0 && speed > -0.1:
+            $("#bar7").addClass("bar-light");
+            return 'descentProfile7';
         case speed < -0.1 && speed > -0.2:
             $("#bar8").addClass("bar-light");
-            break;
+            return 'descentProfile6';
         case speed < -0.2 && speed > -0.3:
             $("#bar9").addClass("bar-light");
-            break;
+            return 'descentProfile5';
         case speed < -0.3 && speed > -0.4:
             $("#bar10").addClass("bar-light");
-            break;
+            return 'descentProfile4';
         case speed < -0.4 && speed > -0.5:
             $("#bar11").addClass("bar-light");
-            break;
+            return 'descentProfile3';
         case speed <= -0.5 && speed > -0.8:
             $("#bar12").addClass("bar-light");
-            break;
+            return 'descentProfile2';
         case speed <= -0.8:
             $("#bar13").addClass("bar-light");
-            break;
+            return 'descentProfile1';
         default:
-            // Handle default case
-            break;
+            $("#bar7").addClass("bar-light");
+            return 'descentProfile7';            
     }
 }
 
